211006cfeed0b2a1c5e7f8988ba3722a23e7ac5a
Potatoplz <beneh0710@gmail.com>
2022-09-27 18:24:57 +0900

    [채팅] 주석 추가

--- src/main/java/kr/co/westudy/chat/ChatMessageDTO.java
+++ src/main/java/kr/co/westudy/chat/ChatMessageDTO.java
@@ -7,10 +7,8 @@ import lombok.Data;
 @Data
 
 public class ChatMessageDTO {
-	private int chat_no;
 	private String roomId;
 	private String sendId;
-	private String writer;
 	private String message;
 	private String datetime;
 }
--- src/main/java/kr/co/westudy/config/WebSocketConfig.java
+++ src/main/java/kr/co/westudy/config/WebSocketConfig.java
@@ -14,14 +14,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	//setAllowedOrigins("*")
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/stomp/chat").withSockJS();
+		registry.addEndpoint("/stomp/chat").withSockJS();//ws/chat
 	}
 
 	/* 어플리케이션 내부에서 사용할 path를 지정할 수 있음 */
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.setApplicationDestinationPrefixes("/pub");
-		registry.enableSimpleBroker("/sub");
-		//enableSimpleBroker : 클라이언트로 메시지를 응답해줄 때 prefix를 정의
+		registry.setApplicationDestinationPrefixes("/pub"); //app
+		//클라이언트가 메시지 전송시 맨앞에 /pub이 붙어있으면 broker로 보내짐
+		registry.enableSimpleBroker("/sub"); //topic
+		//prefix로 /sub가 붙으면 메시지브로커가 잡아서 해당 채팅방을 구독하는 클라이언트에게 메시지를 전달
 	}
 }
